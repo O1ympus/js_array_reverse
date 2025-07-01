e1c6baee02c30f9c86e460938b2e5bc92dd71395
keygen_exe <matkovskiyf3vlad@gmail.com>
2025-07-01 18:09:50 +0300

    sol

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,30 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(arrayReverse(['Mate', 'Academy'])).toBeInstanceOf(Array);
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
+    expect(arrayReverse([''])).toEqual(['']);
+  });
+
+  it(`should return an arr with strings
+    with an original length `, () => {
+    expect(arrayReverse(['Mate', 'Academy'])[0].length).toBe(4);
+    expect(arrayReverse(['Mate', 'Academy'])[1].length).toBe(7);
+  });
+
+  it(`should return right res`, () => {
     expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
   });
 
-  // write more tests here
+  it(`should work right with single el`, () => {
+    expect(arrayReverse(['Mate'])).toEqual(['etaM']);
+  });
+
+  it(`should work right with a lot of elements`, () => {
+    expect(arrayReverse(['I', 'am', 'a', 'student!']))
+      .toEqual(['!', 'tn', 'e', 'dutsamaI']);
+  });
 });
